0212ed16b8556db9791ecd6a9c709695855ff9d4
DawidRyczko <ryczko.dawid@gmail.com>
2025-05-09 16:07:00 +0200

    Adjust base path in Astro config to an empty string.
    Changed the `base` configuration from "." to an empty string. This ensures proper URL resolution and compatibility with deployment environments.

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  base: ".",
+  base: "",
   site: "https://zacznijprogramowac.net",
   integrations: [mdx(), sitemap()],
   vite: {
